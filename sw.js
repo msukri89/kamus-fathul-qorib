4d76de2ba439618412ecb2a79858cb01c905efb6
msukri89 <mochammadsyukri@gmail.com>
2026-09-11 22:31:52 +0700

    Bump cache ke v45 dan cache correction Bab Thaharah أَحْكَامٌ

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // UBAH VERSI DI SINI SETIAP KALI ADA PERUBAHAN FILE (HTML/JS/JSON)
-const CACHE_NAME = 'kamus-fq-v44';
+const CACHE_NAME = 'kamus-fq-v45';
 
 const ASSETS_TO_CACHE = [
   './',
@@ -22,6 +22,7 @@ const ASSETS_TO_CACHE = [
   './data-corrections-thaharah-dasar.json',
   './data-corrections-thaharah-air.json',
   './data-corrections-thaharah-air-qc.json',
+  './data-corrections-thaharah-ahkam.json',
   './data-corrections-tathir-julud-maitah.json',
   './data-corrections-awani.json',
   './data-corrections-siwak.json',
