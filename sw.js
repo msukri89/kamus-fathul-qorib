c2a2a26e6e2eeb76fd61660684e214fd372e9a70
msukri89 <mochammadsyukri@gmail.com>
2026-09-11 21:39:30 +0700

    Bump service worker cache for Chapter 3 corrections

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // UBAH VERSI DI SINI SETIAP KALI ADA PERUBAHAN FILE (HTML/JS/JSON)
-const CACHE_NAME = 'kamus-fq-v33';
+const CACHE_NAME = 'kamus-fq-v34';
 
 const ASSETS_TO_CACHE = [
   './',
@@ -23,6 +23,7 @@ const ASSETS_TO_CACHE = [
   './data-corrections-thaharah-air.json',
   './data-corrections-thaharah-air-qc.json',
   './data-corrections-tathir-julud-maitah.json',
+  './data-corrections-awani.json',
   './manifest.json'
 ];
 
