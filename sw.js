1ae5157ecc750b0e99c3e1b98ed1bf326635cccf
msukri89 <mochammadsyukri@gmail.com>
2026-09-12 03:05:59 +0700

    Fix PWA cache to load rebuilt datasets

--- sw.js
+++ sw.js
@@ -1,32 +1,12 @@
 // UBAH VERSI DI SINI SETIAP KALI ADA PERUBAHAN FILE (HTML/JS/JSON)
-const CACHE_NAME = 'kamus-fq-v47';
+const CACHE_NAME = 'kamus-fq-v48';
 
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './app.js',
   './data.json',
-  './data-corrections.json',
-  './data-corrections-wudhu.json',
-  './data-corrections-sunnah-wudhu.json',
-  './data-corrections-nawaqidh-wudhu.json',
-  './data-corrections-mujibat-ghusl.json',
-  './data-corrections-fardhu-ghusl.json',
-  './data-corrections-sunnah-ghusl.json',
-  './data-corrections-aghsaal-sunnah.json',
-  './data-corrections-khuff.json',
-  './data-corrections-tayammum.json',
-  './data-corrections-jabirah.json',
-  './data-corrections-haid-nifas-istihadhah.json',
-  './data-corrections-larangan-haid-junub-hadats.json',
-  './data-corrections-thaharah-dasar.json',
-  './data-corrections-thaharah-air.json',
-  './data-corrections-thaharah-air-qc.json',
-  './data-corrections-thaharah-ahkam.json',
-  './data-corrections-tathir-julud-maitah.json',
-  './data-corrections-awani.json',
-  './data-corrections-siwak.json',
-  './data-corrections-najis.json',
+  './data-bab2.json',
   './manifest.json'
 ];
 
