294f372a8746775c6a970a133c4554a4b6f89d3e
msukri89 <mochammadsyukri@gmail.com>
2026-09-11 21:34:18 +0700

    Naikkan cache PWA ke v32 untuk layer QC Thaharah Air

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // UBAH VERSI DI SINI SETIAP KALI ADA PERUBAHAN FILE (HTML/JS/JSON)
-const CACHE_NAME = 'kamus-fq-v31';
+const CACHE_NAME = 'kamus-fq-v32';
 
 const ASSETS_TO_CACHE = [
   './',
@@ -21,6 +21,7 @@ const ASSETS_TO_CACHE = [
   './data-corrections-larangan-haid-junub-hadats.json',
   './data-corrections-thaharah-dasar.json',
   './data-corrections-thaharah-air.json',
+  './data-corrections-thaharah-air-qc.json',
   './manifest.json'
 ];
 
