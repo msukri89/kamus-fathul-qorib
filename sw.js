cce731e688490a3c0a5ac168e4cd81a0fa08f398
msukri89 <mochammadsyukri@gmail.com>
2026-09-12 10:38:49 +0700

    Integrasikan BAB28 dan naikkan cache ke v85

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // UBAH VERSI DI SINI SETIAP KALI ADA PERUBAHAN FILE (HTML/JS/JSON)
-const CACHE_NAME = 'kamus-fq-v84';
+const CACHE_NAME = 'kamus-fq-v85';
 
 const ASSETS_TO_CACHE = [
   './',
@@ -32,6 +32,7 @@ const ASSETS_TO_CACHE = [
   './data-bab25.json',
   './data-bab26.json',
   './data-bab27.json',
+  './data-bab28.json',
   './manifest.json'
 ];
 
@@ -62,7 +63,8 @@ const DATA_FILES = new Set([
   'data-bab24.json',
   'data-bab25.json',
   'data-bab26.json',
-  'data-bab27.json'
+  'data-bab27.json',
+  'data-bab28.json'
 ]);
 
 self.addEventListener('install', (event) => {
